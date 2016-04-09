f9162ea10ab54e299bf96adfd749e881a4040460
lizzard <jiri.kaipr@gmail.com>
2016-04-09 21:18:51 +0200

    improved ticket list item

--- app/react/tickets/TicketListItem.jsx
+++ app/react/tickets/TicketListItem.jsx
@@ -1,12 +1,21 @@
 import React, { PropTypes } from 'react';
+import { Col, Row } from 'react-bootstrap';
 import { Link } from 'react-router';
 import TicketStatusBadge from './TicketStatusBadge';
 
 const TicketListItem = ({ id, description, user_id, created_at, state, subject }) => (
   <Link to={`/tickets/${id}`} className="list-group-item">
-    <h4 className="list-group-item-heading">{subject}</h4>
-    <TicketStatusBadge state={state} />
-    <span className="label label-default pull-xs-right">{created_at}</span>
+    <Row>
+      <Col sm={6}>
+        <h4 className="list-group-item-heading">{subject}</h4>
+      </Col>
+      <Col sm={3}>
+        <TicketStatusBadge state={state} />
+      </Col>
+      <Col sm={3}>
+        <span className="label label-default pull-xs-right">{created_at}</span>
+      </Col>
+    </Row>
   </Link>
 );
 
