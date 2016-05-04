f96218fc733f0268b27ce4192d5c39f363e97ff2
lizzard <jiri.kaipr@gmail.com>
2016-05-04 18:43:55 +0200

    added request action factory

--- app/react/app/factories/createRequestActionTypes.js
+++ app/react/app/factories/createRequestActionTypes.js
@@ -2,8 +2,11 @@ const REQUEST = 'REQUEST';
 const SUCCESS = 'SUCCESS';
 const FAILURE = 'FAILURE';
 
-export default (base) =>
-  [REQUEST, SUCCESS, FAILURE].reduce((requestTypes, type) => {
+const makeActions = (base, types) =>
+  types.reduce((requestTypes, type) => {
     requestTypes[type] = `${base}_${type}`;
     return requestTypes;
   }, {});
+
+export default (base) => makeActions(base, [REQUEST, SUCCESS, FAILURE]);
+export const request = (base) => makeActions(base, [REQUEST]);
